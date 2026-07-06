2c407f8a31c8f9d4e23c1dc2dee7a60a4d5abe22
Alexey Kudryavtsev <aleksey.kudryavtsew@gmail.com>
2026-07-06 16:57:51 +0300

    chore(root): initialize monorepo structure

---
+++ backend/src/main/java/io/github/kudryavcAS/mediatracker/MediaTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.kudryavcAS.mediatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTrackerApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/kudryavcAS/mediatracker/MediaTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kudryavcAS.mediatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
